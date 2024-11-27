a4c61ab8b55c9049be04dc10ed51270846e00939
James Smith <james.smith@djalbat.com>
2024-11-27 14:33:31 +0000

    Exported the expression lexer and parser.

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@
 export { default as Query } from "./query";
 export { default as Expression } from "./expression";
 export { default as queryUtilities } from "./utilities/query";
+export { default as ExpressionLexer } from "./expression/lexer";
+export { default as ExpressionParser } from "./expression/parser";

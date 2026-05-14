c7ff3a283d4126d9f2be6af3881251fb2f4ab24d
James Smith <james.smith@djalbat.com>
2026-05-14 22:32:21 +0100

    added to the query utilities.

--- src/utilities/query.js
+++ src/utilities/query.js
@@ -1,7 +1,60 @@
 "use strict";
 
+import { arrayUtilities } from "necessary";
+
 import Query from "../query";
 import Expression from "../expression";
+
+const { push } = arrayUtilities;
+
+export function nodeQuery(expression) {
+  const query = Query.fromExpressionString(expression);
+
+  return function(node) {
+    const nodes = query.execute(node);
+
+    node = nodes.shift() || null; ///
+
+    return node;
+  }
+}
+
+export function nodesQuery(expression) {
+  const query = Query.fromExpressionString(expression);
+
+  return function(node) {
+    const nodes = query.execute(node);
+
+    return nodes;
+  }
+}
+
+export function nodeFromNodeAndQuery(node, query) {
+  const queryNodes = query.execute(node),
+        queryNode = queryNodes.shift() || null;
+
+  node = queryNode;  ///
+
+  return node;
+}
+
+export function nodesFromNodeAndQuery(node, query, nodes = []) {
+  const queryNodes = query.execute(node);
+
+  push(nodes, queryNodes);
+
+  return nodes;
+}
+
+export function nodesFromNodeAndQueries(node, queries, nodes = []) {
+  queries.forEach((query) => {
+    const queryNodes = query.execute(node);
+
+    push(nodes, queryNodes);
+  });
+
+  return nodes;
+}
 
 export function queryByExpressionString(node, expressionString, maximumDepth) {
   let nodes = null;
@@ -18,5 +71,10 @@ export function queryByExpressionString(node, expressionString, maximumDepth) {
 }
 
 export default {
+  nodeQuery,
+  nodesQuery,
+  nodeFromNodeAndQuery,
+  nodesFromNodeAndQuery,
+  nodesFromNodeAndQueries,
   queryByExpressionString
 };
